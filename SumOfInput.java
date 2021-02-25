175405154404e4daaf37fa9b6000cccfb1b025fd
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-02-25 15:31:15 -0500

    Filled out oneToNumber

--- SumOfInput.java
+++ SumOfInput.java
@@ -19,7 +19,10 @@ public class SumOfInput
     }
     
     public Integer oneToNumber(Integer numberToSum){
-
+        sum = 0;
+        for(int i = 0; i <= numberToSum; i++){
+            sum = sum + i;
+        }
         return sum;
     }
 
